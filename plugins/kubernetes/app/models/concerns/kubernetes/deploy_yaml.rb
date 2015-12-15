a3ca87a3a1c679d55650d730b3a79a79eaada42e
Shane Hender <henders@gmail.com>
2015-12-15 11:04:35 +0000

    Use new deployment API.

--- plugins/kubernetes/app/models/concerns/kubernetes/deploy_yaml.rb
+++ plugins/kubernetes/app/models/concerns/kubernetes/deploy_yaml.rb
@@ -5,20 +5,25 @@ module Kubernetes
         if replication_controller_doc.present?
           JSON.parse(replication_controller_doc).with_indifferent_access
         else
-          template[:spec][:replicas] = replica_target
+          template.spec.replicas = replica_target
+          template.metadata.namespace = deploy_group.kubernetes_namespace
           add_labels
           update_docker_image
           set_resource_usage
+          Rails.logger.info "Created K8S hash: #{template}"
+          template
         end
     end
 
     private
 
     def template
-      @template ||=
-        YAML.load_stream(raw_template, kubernetes_role.config_file).detect do |doc|
+      @template ||= begin
+        yaml = YAML.load_stream(raw_template, kubernetes_role.config_file).detect do |doc|
           doc['kind'] == 'ReplicationController' || doc['kind'] == 'Deployment'
-        end.with_indifferent_access
+        end
+        RecursiveOpenStruct.new(yaml, :recurse_over_arrays => true)
+      end
     end
 
     def raw_template
@@ -26,13 +31,15 @@ module Kubernetes
     end
 
     def add_labels
-      template[:metadata][:labels].merge!(labels)
-      template[:spec][:selector].merge!(labels)
-      template[:spec][:template][:metadata][:labels].merge!(labels)
+      labels.each do |key, value|
+        template.metadata.labels[key] = value
+        template.spec.selector[key] = value
+        template.spec.template.metadata.labels[key] = value
+      end
     end
 
     def set_resource_usage
-      container_hash[:resources] = {
+      container.resources = {
         limits: { cpu: kubernetes_role.cpu, memory: kubernetes_role.ram_with_units }
       }
     end
@@ -44,8 +51,11 @@ module Kubernetes
     def update_docker_image
       docker_path = build.docker_repo_digest || "#{build.project.docker_repo}:#{build.docker_ref}"
       # Assume first container is one we want to update docker image in
-      container_hash = template[:spec][:template][:spec][:containers].first
-      container_hash[:image] = docker_path
+      container.image = docker_path
+    end
+
+    def container
+      template.spec.template.spec.containers.first
     end
   end
 end
